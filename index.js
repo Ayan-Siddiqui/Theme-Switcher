a50de58100f22a954865614bab3546858047f16e
Ayan-Siddqui <ayansiddiqui128@gmail.com>
2024-10-13 19:52:23 +0530

    index.js modified

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const buttons = document.querySelectorAll(".button")
 const body = document.querySelector("body")
 
 const handleClick = (e) => {
-    if(e.target.id === "yellow"){
-        body.style.backgroundColor = "#fde047"
-    }
-    if(e.target.id === "blue"){
-        body.style.backgroundColor = "#7dd3fc"
-    }
-    if(e.target.id === "white"){
-        body.style.backgroundColor = "#ffffff"
-    }
-    if(e.target.id === "red"){
-        body.style.backgroundColor = "#fda4af"
-    }
-    if(e.target.id === "purple"){
-        body.style.backgroundColor = "#d8b4fe"
-    }
+    // if(e.target.id === "yellow"){
+    //     
+    // }
+    // if(e.target.id === "blue"){
+    // }
+    // if(e.target.id === "white"){
+    //     
+    // }
+    // if(e.target.id === "red"){
+    //     
+    // }
+    // if(e.target.id === "purple"){
+    //     
+    // }
     // body.style.backgroundColor = e.target.id
+    switch(e.target.id){
+        case "yellow":
+            body.style.backgroundColor = "#fde047"
+            break
+        case "blue":
+            body.style.backgroundColor = "#7dd3fc"
+            break
+        case "white":
+            body.style.backgroundColor = "#ffffff"
+            break
+        case "red":
+            body.style.backgroundColor = "#fda4af"
+            break
+        case "purple":
+            body.style.backgroundColor = "#d8b4fe"
+            break
+    }
 }
 
 buttons.forEach((button) => {
